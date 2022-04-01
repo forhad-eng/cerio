c47b7a65486cd3e15be0e5eef98e3ca8dc777869
forhad-eng <forhadu.cse@gmail.com>
2022-04-02 01:30:55 +0600

    header completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import { MenuIcon, SearchIcon, ShoppingCartIcon, UserIcon, XIcon } from '@heroicons/react/solid'
+import React, { useState } from 'react'
+import CustomLink from '../CustomLink/CustomLink'
+import logo from '../images/logo.webp'
+import './Header.css'
+
+const Header = () => {
+    const [open, setOpen] = useState(true)
+
+    const handleIcon = () => {
+        setOpen(!open)
+    }
+    return (
+        <header>
+            <div onClick={handleIcon} className="md:hidden h-8 w-8 absolute top-4 left-3">
+                {open ? <MenuIcon /> : <XIcon />}
+            </div>
+            <nav className="`w-full flex justify-around md:justify-between items-center p-4 md:p-7 md:static">
+                <div className="md:flex md:justify-around">
+                    <img src={logo} alt="" />
+                    <div
+                        className={`md:flex md:items-center pl-5 md:pl-16 gap-5 font-semibold text-lg md:static absolute left-0 text-left duration-300 ease-in bg-fuchsia-800 md:bg-white w-full ${
+                            !open ? 'top-14' : 'top-[-120px]'
+                        }`}
+                    >
+                        <CustomLink to="/">Home</CustomLink>
+                        <CustomLink to="/shop">Shop</CustomLink>
+                        <CustomLink to="/review">Review Cart</CustomLink>
+                        <CustomLink to="/about">About</CustomLink>
+                    </div>
+                </div>
+                <div className="flex gap-5">
+                    <SearchIcon className="h-6 w-6" />
+                    <UserIcon className="h-6 w-6" />
+                    <ShoppingCartIcon className="h-6 w-6" />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
