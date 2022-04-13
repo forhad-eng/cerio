fbfb2539935a5aab959bdf68cee5c217bb0d22d9
forhad-eng <forhadu.cse@gmail.com>
2022-04-13 21:43:59 +0600

    firebase credentials embedded into react env variables

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyC04XhawLHMEIEMrRcHuIQTtiZCHjeio84',
-    authDomain: 'cerio-shop.firebaseapp.com',
-    projectId: 'cerio-shop',
-    storageBucket: 'cerio-shop.appspot.com',
-    messagingSenderId: '1006264188516',
-    appId: '1:1006264188516:web:28941b751464164b0d6971'
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 export default auth
